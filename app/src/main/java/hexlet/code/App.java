6a03719d08b5364333b5cb001fbf1005c769f839
Temporal Form <leheupofall@yandex.ru>
2024-03-16 21:51:07 +0300

    Add option to choose game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the brain games!");
-        Cli.greetUser();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit
+                """);
+        String choiceNumber = scanner.next();
+        System.out.println("Your choice: " + choiceNumber);
+        switch (Integer.parseInt(choiceNumber)) {
+            case 1:
+                System.out.println("Welcome to the brain games!");
+                Cli.greetUser();
+                break;
+            case 0:
+            default:
+                scanner.close();
+                break;
+        }
     }
 }
